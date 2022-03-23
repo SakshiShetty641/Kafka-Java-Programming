6e590dba37fde5791bbdbe5660afe31b0afeb749
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-03-23 11:08:57 +0530

    To demonstrate consumer subscribing to the topic

---
+++ src/main/java/com/practice/kafka/ConsumerDemo.java
@@ -0,0 +1,55 @@
+package com.practice.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemo {
+    static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Consumer");
+
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "my_second_app";
+        String topic = "Demo_Topic";
+
+        //TODO - Create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");  //To start reading messages from the start and earliest
+
+
+        //TODO - create the consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+
+        //TODO - Subscribe Consumer to our topic
+        consumer.subscribe(Collections.singletonList(topic));
+
+
+        //TODO - POLLING for new data
+        while (true) {
+            log.info("polling");
+
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000)); //it will display the data the immediately or will wait for the time mentioned and then display the data
+
+            for (ConsumerRecord<String, String> record : records) {
+                log.info("key :" + record.key() + " , value" + record.value());
+                log.info("Partition :" + record.partition() + " , offset" + record.partition());
+            }
+        }
+    }
+
+}
--- src/main/java/com/practice/kafka/KafkaConsumer.java
+++
@@ -1,2 +0,0 @@
-package com.practice.kafka;public class KafkaConsumer {
-}
